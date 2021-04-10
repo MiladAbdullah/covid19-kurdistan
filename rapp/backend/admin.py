afb731343cd4c2630924349f8d12ff72a6e30840
MiladAbdullah <milad.chey@gmail.com>
2021-04-10 14:34:16 +0300

    Changes

--- rapp/backend/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Patient, Region,Disease,Sign
-
-admin.site.register(Patient)
-admin.site.register(Region)
-admin.site.register(Disease)
-admin.site.register(Sign)
-# Register your models here.
